6d64d8232b6a19ddd301db746e2841ee07ef6775
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2025-12-17 12:11:31 +0800

    Add function to get user's choice

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //console.log("Hello, World!");
+const CHOICES = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
-    return choices[randomNumber];
+    //console.log(randomNumber);
+    return CHOICES[randomNumber];
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock (1), Paper (2), or Scissors (3)? \nEnter the corresponding number: ");
+    return CHOICES[humanChoice - 1];
 }
