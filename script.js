d3c993a0a44a3135f57eabc0d879ffa16fcb7607
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2025-12-17 12:26:41 +0800

    Add function to play 1 round of RPS
    Shows result every time the function is called.
    Example: Human 1 | Computer 0

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //console.log("Hello, World!");
 const CHOICES = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -11,3 +13,35 @@ function getHumanChoice() {
     let humanChoice = prompt("Rock (1), Paper (2), or Scissors (3)? \nEnter the corresponding number: ");
     return CHOICES[humanChoice - 1];
 }
+
+function playRound(humanChoice, computerChoice) {
+    // rock beats scissors
+    // scissors beats paper
+    // paper beats rock
+
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+    }
+    if (computerChoice == "rock" && humanChoice == "scissors") {
+        computerScore++;
+    }
+    else if (computerChoice == "scissors" && humanChoice == "paper") {
+        computerScore++;
+    }
+    else if (computerChoice == "paper" && humanChoice == "rock") {
+        computerScore++;
+    }
+
+    console.log("Human " + humanScore + " | Computer " + computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
